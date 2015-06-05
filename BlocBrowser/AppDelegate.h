4da6c75bff1ce01cff3c539912903a099d7dbf27
Levi Kennedy <lckennedy@gmail.com>
2015-06-04 21:53:56 -0500

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Levi Kennedy on 6/4/15.
+//  Copyright (c) 2015 Levi Kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
